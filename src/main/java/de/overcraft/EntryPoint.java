a62baef9b7e693f0ca91d43416636e23a1be0d0b
mondgnu <mondgnu@gmail.com>
2023-07-21 20:55:34 +0200

    Fixed public token, now loaded from file

--- src/main/java/de/overcraft/EntryPoint.java
+++ src/main/java/de/overcraft/EntryPoint.java
@@ -5,11 +5,23 @@ import de.overcraft.command.SlashCommandRegister;
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
 
+import java.io.*;
+import java.nio.CharBuffer;
 import java.util.Set;
 
 public class EntryPoint {
     public static void main(String[] args) {
-        DiscordApi api = new DiscordApiBuilder().setToken("MTEzMTU4NTQ0MTY3ODc2NjIyMQ.GUB12y.cfeYBW7x901JAf1Up8oqYAiZOlvwGG9beduVD4").login().join();
+        File file = new File("../config/token.txt");
+        String token;
+        try {
+            BufferedReader fileReader = new BufferedReader(new FileReader(file));
+            token = fileReader.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        if(token.isBlank() || token.isEmpty())
+            throw new RuntimeException("no token found");
+        DiscordApi api = new DiscordApiBuilder().setToken(token).login().join();
         new Bot(api);
     }
 
