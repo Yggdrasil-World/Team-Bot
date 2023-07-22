8bd1170cda15b345995fe0918a55329d80017d07
mondgnu <mondgnu@gmail.com>
2023-07-23 01:50:37 +0200

    Added Infrastructure for Actions

---
+++ src/main/java/de/overcraft/actions/InviteAction.java
@@ -0,0 +1,11 @@
+package de.overcraft.actions;
+
+import org.javacord.api.DiscordApi;
+
+public class InviteAction implements Action<Invite, Invite>{
+
+    @Override
+    public Invite handle(ActionData<Invite> data) {
+        return null;
+    }
+}
