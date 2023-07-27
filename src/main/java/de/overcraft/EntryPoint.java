258ba16d2ddd901e44ef6ed1e20fe874281d7ff9
mondgnu <mondgnu@gmail.com>
2023-07-27 13:41:09 +0200

    added dotenv to project, discord api token is now loaded from .env file

--- src/main/java/de/overcraft/EntryPoint.java
+++ src/main/java/de/overcraft/EntryPoint.java
@@ -1,23 +1,16 @@
 package de.overcraft;
 
 
+import io.github.cdimascio.dotenv.Dotenv;
+import io.github.cdimascio.dotenv.DotenvBuilder;
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
 import java.io.*;
 
 public class EntryPoint {
     public static void main(String[] args) {
-        File file = new File("../config/token.txt");
-        String token;
-        try {
-            BufferedReader fileReader = new BufferedReader(new FileReader(file));
-            token = fileReader.readLine();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        if(token.isBlank() || token.isEmpty())
-            throw new RuntimeException("no token found");
-        DiscordApi api = new DiscordApiBuilder().setToken(token).login().join();
+        Dotenv dotenv = new DotenvBuilder().load();
+        DiscordApi api = new DiscordApiBuilder().setToken(dotenv.get("DISCORD_API_KEY")).login().join();
         new Bot(api);
     }
 
